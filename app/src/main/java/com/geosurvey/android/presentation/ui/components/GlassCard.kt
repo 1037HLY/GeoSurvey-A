7662599fdaefd4742fb0b5f2a8de36fa80767c04
1037HLY <hs202027323@gmail.com>
2026-08-02 23:10:17 +0800

    Update GlassCard.kt

--- app/src/main/java/com/geosurvey/android/presentation/ui/components/GlassCard.kt
+++ app/src/main/java/com/geosurvey/android/presentation/ui/components/GlassCard.kt
@@ -2,6 +2,7 @@ package com.geosurvey.android.presentation.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
