75bb86b0d9c23d7b394f58e1d3e57181e5f0e0b5
1037HLY <hs202027323@gmail.com>
2026-08-02 20:41:17 +0800

    feat: 注册产状实体和DAO

--- app/src/main/java/com/geosurvey/android/data/database/AppDatabase.kt
+++ app/src/main/java/com/geosurvey/android/data/database/AppDatabase.kt
@@ -3,14 +3,19 @@ package com.geosurvey.android.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.geosurvey.android.data.model.AttitudeRecord
 import com.geosurvey.android.data.model.TrackPoint
 
 @Database(
-    entities = [TrackPoint::class],
-    version = 1,
+    entities = [
+        TrackPoint::class,
+        AttitudeRecord::class
+    ],
+    version = 2,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun trackPointDao(): TrackPointDao
+    abstract fun attitudeDao(): AttitudeDao
 }
